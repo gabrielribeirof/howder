03505283a72ed9a5a2355c0f3de32e3a4e445ab4
gabrielribeirof <ogabrielribeirof@gmail.com>
2021-03-31 00:27:04 -0300

    feat(public/socket.js): create socket.io control object

---
+++ public/socket.js
@@ -0,0 +1,63 @@
+const Socket = {
+  instance: undefined,
+
+  init() {
+    if (!Socket.instance) {
+      Socket.instance = io()
+    } else {
+      throw new Error('Socket instance already exists')
+    }
+  },
+
+  getInstance() {
+    if (Socket.instance) {
+      return Socket.instance
+    } else {
+      throw new Error('Socket instance doesn\'t exist')
+    }
+  },
+
+  on(eventName, ...args) {
+    Socket.getInstance().on(eventName, ...args)
+  },
+
+  emit(eventName, ...args) {
+    Socket.getInstance().emit(eventName, ...args)
+  },
+
+  join({ nickname }) {
+    Socket.getInstance().emit('join', {
+      nickname
+    })
+  },
+
+  onNewMessage(listener) {
+    Socket.getInstance().on('message:new', data => {
+      listener({
+        nickname: data.nickname,
+        content: data.content,
+        relation: 'received'
+      })
+    })
+  },
+
+  onJoin(listener) {
+    Socket.getInstance().on('log:join', data => {
+      listener({
+        content: data.nickname,
+        relation: 'join'
+      })
+    })
+  },
+
+  onLeft(listener) {
+    Socket.getInstance().on('log:left', data => {
+      listener({
+        content: data.nickname,
+        relation: 'left'
+      })
+    })
+  }
+}
+
+export default Socket
