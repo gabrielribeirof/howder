6bf876d78c2dc448937d628b77849e39e5672762
gabrielribeirof <ogabrielribeirof@gmail.com>
2021-05-04 23:58:10 -0300

    feat(migrations): create chats table migration

---
+++ src/shared/infra/typeorm/migrations/1619420230896-create-chats.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateChats1619420230896 implements MigrationInterface {
+  private table = new Table({
+    name: 'chats',
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true
+      },
+      {
+        name: 'user_id',
+        type: 'varchar'
+      },
+      {
+        name: 'agent_id',
+        type: 'varchar'
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'now()'
+      }
+    ],
+    foreignKeys: [
+      {
+        name: 'FKUser',
+        columnNames: ['user_id'],
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE'
+      },
+      {
+        name: 'FKAgent',
+        columnNames: ['agent_id'],
+        referencedTableName: 'agents',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE'
+      }
+    ]
+  })
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table)
+  }
+}
