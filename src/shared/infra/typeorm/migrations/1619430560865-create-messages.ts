1aae4be338216f586f2aea6ad7a36d271f6c9a2e
gabrielribeirof <ogabrielribeirof@gmail.com>
2021-05-06 23:23:45 -0300

    feat(migrations): create messages table

---
+++ src/shared/infra/typeorm/migrations/1619430560865-create-messages.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateMessages1619430560865 implements MigrationInterface {
+  private table = new Table({
+    name: 'messages',
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true
+      },
+      {
+        name: 'chat_id',
+        type: 'uuid'
+      },
+      {
+        name: 'is_agent',
+        type: 'boolean'
+      },
+      {
+        name: 'content',
+        type: 'varchar'
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'now()'
+      }
+    ],
+    foreignKeys: [
+      {
+        name: 'FKChat',
+        columnNames: ['chat_id'],
+        referencedTableName: 'chats',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      }
+    ]
+  })
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table)
+  }
+}
