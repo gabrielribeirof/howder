de0b8ba57ca690ef357c6f04ac030460e797f507
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2021-06-04 19:14:39 -0300

    feat(shared/domain): remove irrelevant type checking from value object

--- src/shared/domain/value-object.ts
+++ src/shared/domain/value-object.ts
@@ -1,19 +1,11 @@
 export abstract class ValueObject<T> {
-  protected props: T
+  protected readonly props: T
 
   constructor(props: T) {
-    this.props = { ...props }
+    this.props = props
   }
 
   public equals(vo: ValueObject<T>): boolean {
-    if (vo === null || vo === undefined) {
-      return false
-    }
-
-    if (vo.props === undefined) {
-      return false
-    }
-
     return JSON.stringify(this.props) === JSON.stringify(vo.props)
   }
 }
