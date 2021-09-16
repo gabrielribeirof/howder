5009829d64a1209b48f3deb451c1ccef6dc2103a
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2021-09-16 12:38:03 -0300

    feat(core/errors): rename email already registered error

--- src/shared/core/errors/services/email-already-registered.error.ts
+++ src/shared/core/errors/services/email-already-registered.error.ts
@@ -1,7 +1,7 @@
 import { ServiceError } from '@shared/core/errors/service-error'
 import { EMAIL_ALREADY_REGISTERED } from '@shared/core/errors/codes.json'
 
-export class AlreadyRegisteredError extends ServiceError {
+export class EmailAlreadyRegisteredError extends ServiceError {
   constructor(field: string, value: string) {
     super({
       code: EMAIL_ALREADY_REGISTERED,
