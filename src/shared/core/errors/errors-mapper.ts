3a6c4703b9a78e04da250c50149a880c8ae4b28b
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2021-10-27 20:18:12 -0300

    feat(core/errors): create errors mapper

---
+++ src/shared/core/errors/errors-mapper.ts
@@ -0,0 +1,25 @@
+import i18next from 'i18next'
+import { DomainError } from '@shared/core/errors/domain-error'
+
+interface IFieldError {
+  code: string
+  message: string
+}
+
+export class ErrorsMapper {
+  public static toInfrastructure(domainErrors: DomainError[]): Record<any, any> {
+    const parsed: Record<string, IFieldError> = {}
+
+    for (const value of domainErrors) {
+      parsed[value.field] = {
+        code: value.code,
+        message: i18next.t(`codes:${value.code}`, {
+          min: String(value.min),
+          max: String(value.max)
+        })
+      }
+    }
+
+    return parsed
+  }
+}
