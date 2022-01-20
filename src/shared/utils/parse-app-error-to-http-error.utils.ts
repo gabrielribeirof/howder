25ef705d03095e0dbc4e95dec6086fa798653762
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-01-20 14:36:31 -0300

    feat(utils): create app error to http error parser

---
+++ src/shared/utils/parse-app-error-to-http-error.utils.ts
@@ -0,0 +1,22 @@
+import i18next from 'i18next'
+import { AppError } from '@shared/core/errors/app-error'
+import { HTTPError } from '@shared/core/errors/http-error'
+
+export function parseAppErrorToHTTPError(appError: AppError): HTTPError {
+  return {
+    status: appError.httpStatusCode,
+    code: appError.code,
+    message: appError.message,
+    violations: appError.violations?.map(value => {
+      return {
+        reason: value.reason,
+        field: value.field,
+        message: i18next.t(`violation_messages:${value.reason}`, {
+          max: value.max,
+          min: value.min,
+          expectedValues: value.expectedValues
+        })
+      }
+    })
+  }
+}
