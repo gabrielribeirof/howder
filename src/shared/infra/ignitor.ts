5918fe85d8ab15d318d814d7b5cf23c19c558c8b
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-01-28 23:59:54 -0300

    feat(ignitor.ts): implement onSigterm util function

--- src/shared/infra/ignitor.ts
+++ src/shared/infra/ignitor.ts
@@ -2,34 +2,37 @@ import 'reflect-metadata'
 import dotenv from 'dotenv'
 
 import { I18n } from './i18n'
-import { TypeORMConnection } from './database/connection'
+import { TypeORMConnection } from './typeorm/connection'
 import { HttpServer } from './http/server'
 import { WsServer } from './ws/server'
 
 import { Logger } from '@shared/core/logger'
+import { onSigterm } from '@shared/utils/on-sigterm.utils'
+
+const logger = new Logger('Ignitor')
 
 async function ignitor(): Promise<void> {
-  const logger = new Logger('Ignitor')
-
-  logger.info(`Application igniting in ${process.env.NODE_ENV}`)
-
-  process.env.NODE_ENV === 'production' && dotenv.config()
+  process.env.NODE_ENV === 'production'
+    ? dotenv.config({ path: '.env.production' })
+    : dotenv.config()
 
   const i18N = new I18n()
   const typeORMConnection = new TypeORMConnection()
   const httpServer = new HttpServer()
   const wsServer = new WsServer()
 
-  try {
-    await i18N.init()
-    await typeORMConnection.create()
-    httpServer.create()
-    wsServer.create(httpServer.instance())
+  await i18N.init()
+  await typeORMConnection.create()
+  httpServer.create()
+  wsServer.create(httpServer.instance())
 
-    logger.info('Application ignited')
-  } catch (error) {
-    logger.emerg(error)
-  }
+  onSigterm(
+    typeORMConnection.close,
+    httpServer.close,
+    wsServer.close
+  )
 }
 
 ignitor()
+  .then(() => logger.info('Application ignited'))
+  .catch(error => logger.emerg(error))
