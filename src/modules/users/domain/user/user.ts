bed6245fd9b633e4e5889b96396d3fe4c38ba339
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-02-08 23:19:40 -0300

    feat(users/domain): separate user data structure

--- src/modules/users/domain/user/user.ts
+++ src/modules/users/domain/user/user.ts
@@ -3,6 +3,8 @@ import { Identifier } from '@shared/core/domain/identifier'
 import { Violation } from '@shared/core/errors/violation'
 import { Either, left, right, combineLefts } from '@shared/core/logic/either'
 
+import { UserData } from './user-data'
+
 import { Name } from '@shared/domain/name'
 import { Email } from '@shared/domain/email'
 
@@ -11,32 +13,30 @@ interface UserProperties {
   email: Email
 }
 
-interface UserData {
-  name: string,
-  email: string
-}
-
 export class User extends AggregateRoot<UserProperties> {
-  public readonly name: Name
-  public readonly email: Email
-
-  private constructor(props: UserProperties, id?: Identifier) {
-    super(id)
-    this.name = props.name
-    this.email = props.email
+  public get name(): Name {
+    return this.properties.name
   }
 
-  public static create(props: UserData, id?: Identifier): Either<Violation[], User> {
-    const nameOrError = Name.create(props.name)
-    const emailOrError = Email.create(props.email)
+  public get email(): Email {
+    return this.properties.email
+  }
 
-    if (nameOrError.isLeft() || emailOrError.isLeft()) {
-      return left(combineLefts(nameOrError, emailOrError))
+  private constructor(properties: UserProperties, id?: Identifier) {
+    super(properties, id)
+  }
+
+  public static create(data: UserData, id?: Identifier): Either<Violation[], User> {
+    const nameResult = Name.create(data.name)
+    const emailResult = Email.create(data.email)
+
+    if (nameResult.isLeft() || emailResult.isLeft()) {
+      return left(combineLefts(nameResult, emailResult))
     }
 
     return right(new User({
-      name: nameOrError.value,
-      email: emailOrError.value
+      name: nameResult.value,
+      email: emailResult.value
     }, id))
   }
 }
