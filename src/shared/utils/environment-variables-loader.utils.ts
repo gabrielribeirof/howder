c6d4b20268ea5f0ed6211e2b553c273af2f6037f
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-02 23:52:29 -0300

    feat(utils): create env-vars loader

---
+++ src/shared/utils/environment-variables-loader.utils.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+import appRootPath from 'app-root-path'
+
+import { Guard } from '@shared/core/logic/guard'
+
+export function environmentVariablesLoader(): void {
+  if (Guard.isOneOf(String(process.env.NODE_ENV), ['development', 'production']).fail) {
+    throw new Error('NODE_ENV environment variable must be \'development\' or \'production\'')
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: `${appRootPath.path}/.env.dev` })
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: `${appRootPath.path}/.env` })
+  }
+}
