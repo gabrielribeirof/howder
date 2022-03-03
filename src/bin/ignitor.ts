6e8b1421dd1dc716c6dc026afbcc5d5802e14ceb
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-02 23:58:48 -0300

    feat(ignitor.ts): implement env-vars loader and shutdown utils

--- src/bin/ignitor.ts
+++ src/bin/ignitor.ts
@@ -1,39 +1,40 @@
 import 'reflect-metadata'
-import dotenv from 'dotenv'
-import appRootPath from 'app-root-path'
 
+import { Logger } from '@shared/core/logger'
 import { I18n } from '@shared/infra/i18n'
 import { TypeORMConnection } from '@shared/infra/typeorm/connection'
 import { HttpServer } from '@shared/infra/http/server'
 import { WsServer } from '@shared/infra/ws/server'
 
-import { Logger } from '@shared/core/logger'
-import { onSigterm } from '@shared/utils/on-sigterm.utils'
+import { beforeShutdown } from '@shared/utils/before-shutdown.utils'
+import { environmentVariablesLoader } from '@shared/utils/environment-variables-loader.utils'
 
 const logger = new Logger('Ignitor')
+const i18n = new I18n()
+const typeORMConnection = new TypeORMConnection()
+const httpServer = new HttpServer()
+const wsServer = new WsServer()
 
 async function ignitor(): Promise<void> {
-  process.env.NODE_ENV === 'production'
-    ? dotenv.config({ path: `${appRootPath}/.env.production` })
-    : dotenv.config()
+  environmentVariablesLoader()
 
-  const i18n = new I18n()
-  const typeORMConnection = new TypeORMConnection()
-  const httpServer = new HttpServer()
-  const wsServer = new WsServer()
+  logger.notice(`Running on ${process.env.NODE_ENV} environment`)
 
   await i18n.init()
   await typeORMConnection.create()
   httpServer.create()
-  wsServer.create(httpServer.instance())
+  wsServer.create(httpServer.instance)
 
-  onSigterm(
-    typeORMConnection.close,
-    httpServer.close,
-    wsServer.close
-  )
+  await httpServer.start()
 }
 
 ignitor()
   .then(() => logger.info('Application ignited'))
+  .then(() => {
+    beforeShutdown(async () => {
+      wsServer.close()
+      await httpServer.close()
+      await typeORMConnection.close()
+    })
+  })
   .catch(error => logger.emerg(error))
