0137754de4a51c9a6bbb18fb9b29e3c992b678f9
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-10 00:03:05 -0300

    test(agents/password): refactor password value object

--- src/modules/agents/domain/agent/password.spec.ts
+++ src/modules/agents/domain/agent/password.spec.ts
@@ -1,65 +1,59 @@
 import bcrypt from 'bcryptjs'
 import { Password } from './password'
 
-const genericPasswordResult = Password.create({ value: '123456', hashed: false })
+let genericPassword: Password
+let hashedPassword: Password
+let hashedValue: string
 
 describe('Agent password value object', () => {
+  beforeAll(async () => {
+    const toGenericPassword = Password.create({ value: '123456', hashed: false })
+    if (toGenericPassword.isLeft()) throw new Error()
+
+    const toHashedValue = await toGenericPassword.value.getHashedValue()
+
+    const toHashedPassword = Password.create({ value: toHashedValue, hashed: true })
+    if (toHashedPassword.isLeft()) throw new Error()
+
+    genericPassword = toGenericPassword.value
+    hashedValue = toHashedValue
+    hashedPassword = toHashedPassword.value
+  })
+
   it('should accept valid password', () => {
-    expect(genericPasswordResult.isRight()).toBeTruthy()
+    const password = Password.create({ value: '123456', hashed: false })
+
+    expect(password.isRight()).toBeTruthy()
   })
 
   it('should reject password with less than 6 characters', () => {
-    const passwordResult = Password.create({ value: '12345', hashed: false })
+    const password = Password.create({ value: '12345', hashed: false })
 
-    expect(passwordResult.isLeft()).toBeTruthy()
+    expect(password.isLeft()).toBeTruthy()
   })
 
   it('should reject password with more than 128 characters', () => {
-    const passwordResult = Password.create({
-      value: '1'.repeat(200),
+    const password = Password.create({
+      value: '1'.repeat(130),
       hashed: false
     })
 
-    expect(passwordResult.isLeft()).toBeTruthy()
+    expect(password.isLeft()).toBeTruthy()
   })
 
-  it('should be able to hash the password', async () => {
-    if (genericPasswordResult.isLeft()) {
-      throw new Error()
-    }
-
-    const hashedPassword = await genericPasswordResult.value.getHashedValue()
-
-    expect(await bcrypt.compare('123456', hashedPassword)).toBeTruthy()
+  it('should hash the password', async () => {
+    expect(await bcrypt.compare('123456', hashedValue)).toBeTruthy()
   })
 
   it('should not hash the password when already hashed', async () => {
-    const hashedPassword = await bcrypt.hash('123456', 8)
-    const passwordResult = Password.create({ value: hashedPassword, hashed: true })
-
-    if (passwordResult.isLeft()) {
-      throw new Error()
-    }
-
-    expect(await passwordResult.value.getHashedValue()).toEqual(hashedPassword)
+    expect(await hashedPassword.getHashedValue()).toEqual(hashedValue)
   })
 
-  it('should be able to compare the password when not hashed', () => {
-    if (genericPasswordResult.isLeft()) {
-      throw new Error()
-    }
-
-    expect(genericPasswordResult.value.comparePassword('123456')).toBeTruthy()
+  it('should compare the password when not hashed', () => {
+    expect(genericPassword.comparePassword('123456')).toBeTruthy()
   })
 
-  it('should be able to compare the password when hashed', async () => {
-    const hashedPassword = await bcrypt.hash('123456', 8)
-    const passwordResult = Password.create({ value: hashedPassword, hashed: true })
-
-    if (passwordResult.isLeft()) {
-      throw new Error()
-    }
-
-    expect(passwordResult.value.comparePassword('123456')).toBeTruthy()
+  it('should compare the password when hashed', async () => {
+    expect(hashedPassword.comparePassword('123456')).toBeTruthy()
   })
 })
