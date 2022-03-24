0f36865517806681896d14c3c5ebb55fa85c4a98
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-24 19:40:10 -0300

    feat(chat/domain): outsources chat factory

---
+++ src/modules/chats/domain/chat/factories/chat.factory.ts
@@ -0,0 +1,30 @@
+import { Violation } from '@shared/core/errors/violation'
+import { Identifier } from '@shared/core/domain/identifier'
+import { Either } from '@shared/core/logic/either'
+
+import { Chat } from '../chat'
+import { ChatTag } from '../chat-tag'
+import { ChatTags } from '../chat-tags'
+
+type CreateChatRequest = {
+  user_id: string
+  member_id?: string
+  workspace_id: string
+  tags?: ChatTag[]
+  is_open?: boolean
+}
+
+export function createChat(properties: CreateChatRequest): Either<Violation[], Chat> {
+  const user_id = new Identifier(properties.user_id)
+  const workspace_id = new Identifier(properties.workspace_id)
+  const member_id = properties.member_id ? new Identifier(properties.member_id) : undefined
+  const tags = ChatTags.create(properties.tags)
+
+  return Chat.create({
+    user_id,
+    member_id,
+    workspace_id,
+    tags,
+    is_open: properties.is_open ?? true
+  })
+}
