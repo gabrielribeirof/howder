b55cd662aafa7f39a559eb71a60f5f846fd3da66
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-24 19:54:02 -0300

    chore(agents/domain): remove old team

--- src/modules/agents/domain/team/team-data.ts
+++
@@ -1,3 +0,0 @@
-export interface TeamData {
-  name: string
-}
--- src/modules/agents/domain/team/team.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Team } from './team'
-
-describe('Team aggregate root', () => {
-  it('should create new team', async () => {
-    const teamResult = Team.create({ name: 'Some team name' })
-
-    expect(teamResult.isRight()).toBeTruthy()
-  })
-})
