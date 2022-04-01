d03d635cc20d7f082ab4b575d2b26b7b248bfbef
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-03-31 23:55:51 -0300

    feat(utils): remove unused ok function from http response utils

--- packages/api/src/shared/utils/http-response.utils.ts
+++ packages/api/src/shared/utils/http-response.utils.ts
@@ -1,11 +1,6 @@
 import { Response } from 'express'
 import { AppError } from '@shared/core/errors/app-error'
-
 import { parseAppErrorToHTTPError } from './parse-app-error-to-http-error.utils'
-
-export function ok(expressResponse: Response, payload?: string): Response {
-  return expressResponse.status(200).json(payload)
-}
 
 export function fail(expressResponse: Response, payload: AppError): Response {
   return expressResponse.status(Number(payload.httpStatus)).json(parseAppErrorToHTTPError(payload))
