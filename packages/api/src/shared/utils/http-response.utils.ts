7cb318dfd0d0f22857c5437310aa6107bf27ea62
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-04-04 23:55:17 -0300

    feat(api/utils): create controller utils

--- packages/api/src/shared/utils/http-response.utils.ts
+++ packages/api/src/shared/utils/http-response.utils.ts
@@ -1,6 +1,27 @@
 import { Response } from 'express'
+import { Either } from '@shared/core/logic/either'
 import { AppError } from '@shared/core/errors/app-error'
+
+import { InternalError } from '@shared/errors/internal.error'
 import { parseAppErrorToHTTPError } from './parse-app-error-to-http-error.utils'
+
+export function ok(expressResponse: Response, payload?: Record<string, any>): Response {
+  return expressResponse.status(200).json(payload)
+}
+
+ok.either = (expressResponse: Response, either: Either<AppError, any>): void => {
+  if (either.isLeft()) {
+    fail(expressResponse, either.value)
+    return
+  }
+
+  if (either.isRight()) {
+    ok(expressResponse, either.value)
+    return
+  }
+
+  fail(expressResponse, new InternalError())
+}
 
 export function fail(expressResponse: Response, payload: AppError): Response {
   return expressResponse.status(Number(payload.httpStatus)).json(parseAppErrorToHTTPError(payload))
