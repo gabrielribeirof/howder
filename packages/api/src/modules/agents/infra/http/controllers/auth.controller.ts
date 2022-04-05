64fd3eb7947115ebf4e4f2dde9736e5883958a56
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2022-04-04 23:59:42 -0300

    feat(api/agents): create auth controller

--- packages/api/src/modules/agents/infra/http/controllers/auth.controller.ts
+++ packages/api/src/modules/agents/infra/http/controllers/auth.controller.ts
@@ -1,7 +1,31 @@
+import { container } from 'tsyringe'
 import { Request, Response } from 'express'
+import { ok } from '@shared/utils/http-response.utils'
+
+import { AgentSignUpService } from '@modules/agents/services/agent-signup.service'
+import { AgentSignInService } from '@modules/agents/services/agent-signin.service'
 
 export class AuthController {
-  public signin(request: Request, response: Response): void {
+  public async signup(request: Request, response: Response): Promise<void> {
+    const { name, email, password } = request.body
+
+    const service = container.resolve(AgentSignUpService)
+
+    ok.either(response, await service.execute({
+      name,
+      email,
+      password
+    }))
+  }
+
+  public async signin(request: Request, response: Response): Promise<void> {
     const { email, password } = request.body
+
+    const service = container.resolve(AgentSignInService)
+
+    ok.either(response, await service.execute({
+      email,
+      password
+    }))
   }
 }
