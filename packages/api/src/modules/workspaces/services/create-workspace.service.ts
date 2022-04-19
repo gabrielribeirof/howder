6292aba5cec6856e9e8c2c4e5e256df79d281be4
Gabriel Ribeiro <39045611+gabrielribeirof@users.noreply.github.com>
2022-04-19 02:59:25 +0000

    feat(api/workspaces): create workspace creator member

--- packages/api/src/modules/workspaces/services/create-workspace.service.ts
+++ packages/api/src/modules/workspaces/services/create-workspace.service.ts
@@ -3,6 +3,7 @@ import { AppError } from '@shared/core/errors/app-error'
 import { Either, right, left } from '@shared/core/logic/either'
 
 import { IWorkspacesRespository } from '../repositories/iworkspaces.repository'
+import { IMembersRepository } from '../repositories/imembers.repository'
 
 import { InvalidParameterError } from '@shared/errors/invalid-parameter.error'
 
@@ -19,7 +20,9 @@ type CreateWorkspaceRequest = {
 export class CreateWorkspaceService {
   constructor(
     @inject('WorkspacesRespository')
-    private workspacesRepository: IWorkspacesRespository
+    private workspacesRepository: IWorkspacesRespository,
+    @inject('MembersRepository')
+    private membersRepository: IMembersRepository
   ) {}
 
   public async execute({
@@ -39,6 +42,13 @@ export class CreateWorkspaceService {
       workspace_id: workspace.value.id.value,
       is_admin: true
     })
+
+    if (member.isLeft()) {
+      await this.workspacesRepository.delete(workspace.value)
+      return left(new InvalidParameterError(member.value))
+    }
+
+    await this.membersRepository.save(member.value)
 
     return right(workspace.value)
   }
--- packages/api/src/modules/workspaces/services/tests/create-workspace.service.spec.ts
+++ packages/api/src/modules/workspaces/services/tests/create-workspace.service.spec.ts
@@ -1,11 +1,13 @@
 import { CreateWorkspaceService } from '../create-workspace.service'
 
 import { InMemoryWorkspacesRepository } from '../../repositories/in-memory/in-memory-workspaces.repository'
+import { InMemoryMembersRespository } from '@modules/workspaces/repositories/in-memory/in-memory-members.repository'
 
 describe('CreateWorkspaceService', () => {
   it('should create a workspace', async () => {
     const workspacesRespository = new InMemoryWorkspacesRepository()
-    const sut = new CreateWorkspaceService(workspacesRespository)
+    const membersRespository = new InMemoryMembersRespository()
+    const sut = new CreateWorkspaceService(workspacesRespository, membersRespository)
 
     const response = await sut.execute({
       name: 'Workspace Test Name',
